bb26bd5eca84074d90d702c6992bb788469d4997
nikotamajapan <nikotamajapan@gmail.com>
2021-07-01 11:01:58 +0900

    練習18 モンテカルロ・シミュレーション むずい

--- a.py
+++ a.py
@@ -1,5 +1,17 @@
-n = int(input('数> '))
-fac = 1
-for i in range(1,n+1) :
-    fac *= i
-print(str(n)+'! =',fac,sep=' ')
+
+shakkin = int(input('借金> '))
+riritsu = float(input('年利率(%)> '))
+hensai =  int(input('返済額> '))
+total = 0
+month = 0
+while shakkin > hensai :
+    month += 1
+    shakkin = shakkin*(1 + riritsu/12/100) - hensai
+    print(str(month)+'月: 返済額',hensai,'円','残り',\
+    int(shakkin),sep=' ')
+    total += hensai
+month += 1
+shakkin = shakkin*(1 + riritsu/12/100)
+total += shakkin
+print(str(month)+'月: 返済額',int(shakkin),'円','これで完済。','返済総額: ',\
+int(total),'円',sep=' ')
